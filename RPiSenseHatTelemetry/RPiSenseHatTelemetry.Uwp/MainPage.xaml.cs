20d853938f77218ff9b4e2b9a3af2ff4851f4496
Radu Matei <matei.radu94@gmail.com>
2016-10-09 02:37:36 +0300

    Added logic in UWP app

--- RPiSenseHatTelemetry/RPiSenseHatTelemetry.Uwp/MainPage.xaml.cs
+++ RPiSenseHatTelemetry/RPiSenseHatTelemetry.Uwp/MainPage.xaml.cs
@@ -1,17 +1,39 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using System;
+using RPiSenseHatTelemetry.SenseHatCommunication;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using RPiSenseHatTelemetry.CloudCommunication;
+using Newtonsoft.Json;
 
 namespace RPiSenseHatTelemetry.Uwp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private SenseHat _senseHat { get; set; }
+        private IoTHubConnection _iotHubConnection { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            _senseHat = new SenseHat();
+            _iotHubConnection = new IoTHubConnection();
+
+            this.ActivateSenseHat();
+
+            this.Loaded += (sender, e) =>
+            {
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Tick += async (x, y) => await _iotHubConnection.SendEventAsync(JsonConvert.SerializeObject(_senseHat.GetTemperature()));
+                timer.Interval = TimeSpan.FromSeconds(3);
+                timer.Start();
+            };
+        }
+
+        private async void ActivateSenseHat()
+        {
+           await _senseHat.Activate();
         }
     }
 }
