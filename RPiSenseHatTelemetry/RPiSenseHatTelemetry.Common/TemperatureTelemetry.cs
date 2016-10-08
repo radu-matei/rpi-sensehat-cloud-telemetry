32317feb30654b0d3e6e9bd41a9f55b852dfea09
Radu Matei <matei.radu94@gmail.com>
2016-10-09 02:35:05 +0300

    Added basic temperature telemetry

---
+++ RPiSenseHatTelemetry/RPiSenseHatTelemetry.Common/TemperatureTelemetry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RPiSenseHatTelemetry.Common
+{
+    public class TemperatureTelemetry
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public double Temperature { get; set; }
+    }
+}
